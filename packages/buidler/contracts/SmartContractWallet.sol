8b7bde4cbc8d17f89ae8824400a8fcd834d02b7f
skeebuzz <pettibone@gmail.com>
2021-02-17 07:01:06 -0500

    tutorial 1

--- packages/buidler/contracts/SmartContractWallet.sol
+++ packages/buidler/contracts/SmartContractWallet.sol
@@ -17,4 +17,70 @@ contract SmartContractWallet {
     msg.sender.transfer((address(this)).balance);
   }
 
+  function isOwner(address possibleOwner) public view returns (bool) {
+    return (possibleOwner==owner);
+  }
+
+  function updateOwner(address newOwner) public {
+    require(isOwner(msg.sender),"NOT THE OWNER!");
+    owner = newOwner;
+  }
+
+  // limit size of internal wallet
+  uint constant public limit = 0.005 * 10**18;
+
+  fallback() external payable {
+    require(((address(this)).balance) <= limit, "WALLET LIMIT REACHED");
+    console.log(msg.sender,"just deposited",msg.value);
+  }
+
+  // store friends that can do friends shit
+  mapping(address => bool) public friends;
+
+  function updateFriend(address friendAddress, bool isFriend) public {
+    require(isOwner(msg.sender),"NOT THE OWNER!");
+    friends[friendAddress] = isFriend;
+    console.log(friendAddress,"friend bool set to",isFriend);
+    emit UpdateFriend(msg.sender,friendAddress,isFriend);
+  }
+
+  modifier onlyOwner() { // Modifier
+    require(
+      msg.sender == owner,
+      "Only owner can call this."
+    );
+    _;
+  }
+
+  // event declaration
+  event UpdateFriend(address sender, address friend, bool isFriend);
+
+  uint public timeToRecover = 0;
+  uint constant public timeDelay = 120; //seconds
+  address public recoveryAddress;   // recovery address
+
+  function setRecoveryAddress(address _recoveryAddress) public onlyOwner {
+    console.log(msg.sender,"set the recoveryAddress to",recoveryAddress);
+    recoveryAddress = _recoveryAddress;
+  }
+
+  function friendRecover() public {
+    require(friends[msg.sender],"NOT A FRIEND");
+    timeToRecover = block.timestamp + timeDelay;
+    console.log(msg.sender,"triggered recovery",timeToRecover,recoveryAddress);
+  }
+
+  // owner can cancel recovery
+  function cancelRecover() public onlyOwner {
+    timeToRecover = 0;
+    console.log(msg.sender,"canceled recovery");
+  }
+
+  // anyone can call if enough time passed
+  function recover() public {
+    require(timeToRecover>0 && timeToRecover<block.timestamp,"NOT EXPIRED");
+    console.log(msg.sender,"triggered recover");
+    // selfdestruct removes the contract from blockchain and return all funds to recoveryAddress
+    selfdestruct(payable(recoveryAddress));
+  }
 }
